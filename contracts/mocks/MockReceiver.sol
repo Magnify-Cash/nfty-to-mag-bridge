cfc9ac767cb77cbd186e05b2530fe5c281d9501e
VladyslavP5 <vladyslav.p@blaize.tech>
2024-06-24 19:33:20 +0300

    fix(`MockReceiver`): fix sentNativeToBridge func

--- contracts/mocks/MockReceiver.sol
+++ contracts/mocks/MockReceiver.sol
@@ -2,14 +2,17 @@
 
 pragma solidity 0.8.26;
 
-import "../Bridge.sol";
+import "../NFTYBridge.sol";
+
+error TransferError();
 
 contract MockReceiver {
     receive() external payable {
         revert("MockReceiver: revert");
     }
 
-    function sendNativeToBridge(address payable bridge, address token) external payable {
-        Bridge(bridge).send{value: msg.value}(token, address(this), msg.value);
+    function sendNativeToBridge(address payable bridge) external payable {
+        (bool sent, ) = payable(bridge).call{value: msg.value}("");
+        if (!sent) revert TransferError();
     }
 }
