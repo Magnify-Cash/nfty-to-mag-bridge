70de10af9261edc177e0580df94d0f47ce4d6076
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-01 08:08:54 +0300

    script: add script for calc possible total supply

---
+++ scripts/utils/getTotalSupply.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+
+import holdersBsc from "../snapshots/tokenHoldersBsc.json";
+import holdersEth from "../snapshots/tokenHoldersEthereum.json";
+import holdersPolygon from "../snapshots/tokenHoldersPolygon.json";
+
+let totalHoldings: bigint = 0n;
+
+export function getTotalSupply() {
+    for (const user of holdersEth) {
+        totalHoldings += ethers.toBigInt(user.balance);
+    }
+
+    for (const user of holdersBsc) {
+        totalHoldings += ethers.toBigInt(user.balance);
+    }
+
+    for (const user of holdersPolygon) {
+        totalHoldings += ethers.toBigInt(user.balance);
+    }
+
+    const amountForBridge = totalHoldings / 8n;
+    const totalSupply = amountForBridge * 2n;
+
+    return { amountForBridge, totalSupply };
+}
