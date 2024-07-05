f4b2c69b8f1a59b9edaff6453153c4277364a604
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-06 00:31:12 +0300

    scripts: add addresses for mint

--- scripts/mintMockToken.ts
+++ scripts/mintMockToken.ts
@@ -3,14 +3,20 @@ import hre from "hardhat";
 const { ethers } = hre;
 import { addDec } from "../test/helpers";
 
-const to = ["0x89758E3b31DdECaBdBFdf6416d153CE611DF903a", "0x0309004C4fB9943797f5C530abd8cddE564A9fD4"];
+const to = [
+    "0x89758E3b31DdECaBdBFdf6416d153CE611DF903a",
+    "0x0309004C4fB9943797f5C530abd8cddE564A9fD4",
+    "0x35c7371cdF8b8866b5Fd97e9A324F1821300B140",
+    "0xe04Ccb301583eeE3cbCd271ed74E547F8271977b",
+    "0xdEdecb49487AbD56aF4a99e2e3D4C63068658e23"
+];
 
 const mockTokenBase = "0xaC9809c3cdBa4052F39501DEC700fc23776e40AF";
 const mockTokenSepolia = "0x6c28de594318C8AB116Ad5865A7fc4b75a8e1dfe";
 const mockTokenPolygon = "0x0B5d53E3b79e3317A17AD5F61910d4F807eCa56a";
 const mockTokenBSC = "0x5Ce62153Cd1F7Da9099d81b58906C0843886dd5D";
 
-const amountToMint = addDec(1000);
+const amountToMint = addDec(1_000_000);
 
 async function main() {
     const networkName = await getNetworkName();
@@ -22,7 +28,7 @@ async function main() {
         console.log("Minting tokens for", address);
 
         const tx = await token.mintFor(address, amountToMint);
-        await tx.wait();
+        await tx.wait(1);
 
         console.log("Tokens minted for", address);
     }
