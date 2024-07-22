bf47669ed753f68de661573a7239cb2737977370
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-22 18:45:39 +0300

    scripts: edit deploy MAG token script

--- scripts/deployment/deployMAGToken.ts
+++ scripts/deployment/deployMAGToken.ts
@@ -2,7 +2,6 @@ import hre from "hardhat";
 import path from "path";
 
 import { getAddressSaver, verify } from "./utils/helpers";
-import { getTotalAmountForBridge } from "../getTotalAmountForBridge";
 import { addDec } from "../../test/helpers";
 
 const { ethers } = hre;
@@ -10,26 +9,13 @@ const { ethers } = hre;
 // Total supply of tokens to be minted during deployment
 // For default value is 100_000_000 + 18 decimals
 const TOTAL_SUPPLY = addDec(100_000_000);
-const { amountForBridge } = getTotalAmountForBridge();
-
-console.log("Predicted amount for bridge", amountForBridge);
-
-// Address of the bridge contract on BASE network
-const testnetBridge = "";
-const mainnetBridge = "";
-
-const totalSupply = addDec(100_000_000);
 
 async function main() {
     const [deployer] = await ethers.getSigners();
 
-    const bridgeAddress = await getBridgeAddress();
-
     console.log("\n --- Deployed data --- \n");
     console.log("* ", deployer.address, "- Deployer address");
-    console.log("* ", totalSupply, "- Total supply of tokens");
-    console.log("* ", bridgeAddress, "- Address of the bridge contract");
-    console.log("* ", amountForBridge, "- Total amount for the bridge contract");
+    console.log("* ", TOTAL_SUPPLY, "- Total supply of the tokens");
 
     console.log("\n --- ------- ---- --- ");
 
@@ -37,22 +23,14 @@ async function main() {
     const network = (await ethers.getDefaultProvider().getNetwork()).name; // Getting of the current network
     const saveAddress = getAddressSaver(addressesPath, network, true);
 
-    const MAGToken = await ethers.deployContract("MAGToken", [TOTAL_SUPPLY, bridgeAddress], deployer);
+    const MAGToken = await ethers.deployContract("MAGToken", [TOTAL_SUPPLY], deployer);
     await MAGToken.waitForDeployment();
 
     const tokenAddress = MAGToken.target.toString();
 
     saveAddress(await MAGToken.symbol(), tokenAddress, false);
     console.log("Deployment is completed.");
-    await verify(tokenAddress, [TOTAL_SUPPLY, bridgeAddress]);
-}
-
-async function getBridgeAddress() {
-    const chainId = (await ethers.provider.getNetwork()).chainId;
-
-    if (chainId == 84532n) return testnetBridge;
-
-    return mainnetBridge;
+    await verify(tokenAddress, [TOTAL_SUPPLY]);
 }
 
 // This pattern is recommended to be able to use async/await everywhere and properly handle errors.
