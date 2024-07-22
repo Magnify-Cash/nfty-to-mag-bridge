f80ed1571e978e929f698d9d2479ecaa27141600
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-22 19:40:52 +0300

    feat(`MAGToken`): removed pause functionality

--- contracts/MAGToken.sol
+++ contracts/MAGToken.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.26;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 /**
@@ -15,10 +14,7 @@ import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-
  * The one who deploys the contract becomes its administrator.
  * The one who deploys the contract becomes its pauser.
  */
-contract MAGToken is ERC20Permit, ERC20Pausable, AccessControl {
-    /// @notice The role of person that able to stop all token transfers.
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
+contract MAGToken is ERC20Permit, AccessControl {
     /**
      * @notice Initializes contract by setting token name(MAG Token) and token symbol(MAG),
      * transfers total supply to person who deployed smart contract.
@@ -31,38 +27,7 @@ contract MAGToken is ERC20Permit, ERC20Pausable, AccessControl {
      */
     constructor(uint256 _totalSupply) ERC20("MAG Token", "MAG") ERC20Permit("MAG Token") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
 
         _mint(msg.sender, _totalSupply);
-    }
-
-    /**
-     * @notice Pause all token transfers.
-     *
-     * Requirements:
-     *  - The contract should be not paused.
-     *  - Only caller with PAUSER_ROLE can puase.
-     */
-    function pause() public whenNotPaused onlyRole(PAUSER_ROLE) {
-        super._pause();
-    }
-
-    /**
-     * @notice Unpause all token transfers.
-     *
-     * Requirements:
-     *  - The contract should be not paused.
-     *  - Only caller with PAUSER_ROLE can unpause.
-     */
-    function unpause() public whenPaused onlyRole(PAUSER_ROLE) {
-        super._unpause();
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20, ERC20Pausable) {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
