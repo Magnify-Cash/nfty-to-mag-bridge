fa9943557531c23f301df7e9c8e9b9108ec8ad72
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-22 18:44:53 +0300

    feat(`MAGToken`): removed pause logic and constructor

--- contracts/MAGToken.sol
+++ contracts/MAGToken.sol
@@ -14,41 +14,24 @@ import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-
  * Smart contract is NOT upgredeable.
  * The one who deploys the contract becomes its administrator.
  * The one who deploys the contract becomes its pauser.
- * Sets the BRIDGE_ROLE to bridge address.
- * The function _beforeTokenTransfer is overwritten in order to allow bridge to transfer tokens when the contract is paused.
  */
 contract MAGToken is ERC20Permit, ERC20Pausable, AccessControl {
     /// @notice The role of person that able to stop all token transfers.
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
-    /// @notice The role of bridge that able to transfer tokens when the contract is paused.
-    bytes32 public constant BRIDGE_ROLE = keccak256("BRIDGE_ROLE");
-
-    /// @notice The address of the bridge.
-    address public immutable bridge;
-
-    /// Errors.
-    error ZeroAddress();
-    error OnlyBridgeCanTransfer();
 
     /**
      * @notice Initializes contract by setting token name(MAG Token) and token symbol(MAG),
      * transfers total supply to person who deployed smart contract.
      * Person that deployed smart contract becom administrator.
      * Person that deployed smart contract becom pauser.
-     * Set BRIDGE_ROLE to bridge address.
      *
      *
      * Requeirements:
      *  - `msg.sender` should not be zero addresss.
      */
-    constructor(uint256 _totalSupply, address _bridge) ERC20("MAG Token", "MAG") ERC20Permit("MAG Token") {
+    constructor(uint256 _totalSupply) ERC20("MAG Token", "MAG") ERC20Permit("MAG Token") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
-
-        if (_bridge == address(0)) revert ZeroAddress();
-        _grantRole(BRIDGE_ROLE, _bridge);
-        bridge = _bridge;
 
         _mint(msg.sender, _totalSupply);
     }
@@ -80,19 +63,6 @@ contract MAGToken is ERC20Permit, ERC20Pausable, AccessControl {
         address to,
         uint256 amount
     ) internal virtual override(ERC20, ERC20Pausable) {
-        if (paused()) {
-            if (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()) || hasRole(BRIDGE_ROLE, _msgSender())) {
-                _unpause();
-
-                super._beforeTokenTransfer(from, to, amount);
-
-                _pause();
-            } else {
-                revert OnlyBridgeCanTransfer();
-            }
-        }
-        if (!paused()) {
-            super._beforeTokenTransfer(from, to, amount);
-        }
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
