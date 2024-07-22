8c93ce09d18e7b15f27a0a49fb10c6ea1f37915d
VladyslavP5 <vladyslav.p@blaize.tech>
2024-07-22 18:46:16 +0300

    test(`MAGToken`): update tests for contracts update

--- test/MAGToken.test.ts
+++ test/MAGToken.test.ts
@@ -7,11 +7,11 @@ import { ethers, upgrades } from "hardhat";
 import { MAGToken } from "../typechain-types";
 import { addDec } from "./helpers";
 
-describe("MAG Token", function () {
+describe.only("MAG Token", function () {
     let snapshotA: SnapshotRestorer;
 
     // Signers.
-    let deployer: HardhatEthersSigner, user: HardhatEthersSigner, bridge: HardhatEthersSigner;
+    let deployer: HardhatEthersSigner, user: HardhatEthersSigner;
 
     let token: MAGToken;
 
@@ -19,15 +19,14 @@ describe("MAG Token", function () {
 
     before(async () => {
         // Getting of signers.
-        [deployer, user, bridge] = await ethers.getSigners();
+        [deployer, user] = await ethers.getSigners();
 
         // Token deployment.
-        token = await ethers.deployContract("MAGToken", [TOTAL_SUPPLY, bridge], deployer);
+        token = await ethers.deployContract("MAGToken", [TOTAL_SUPPLY], deployer);
         await token.waitForDeployment();
 
         // Transfer tokens to users.
         await token.transfer(user, addDec(1000));
-        await token.transfer(bridge, addDec(1000));
 
         snapshotA = await takeSnapshot();
     });
@@ -42,18 +41,11 @@ describe("MAG Token", function () {
             expect(await token.totalSupply()).to.be.equal(TOTAL_SUPPLY);
 
             // Check balance.
-            expect(await token.balanceOf(deployer)).to.be.equal(TOTAL_SUPPLY - addDec(1000) * 2n);
+            expect(await token.balanceOf(deployer)).to.be.equal(TOTAL_SUPPLY - addDec(1000));
 
             // Check roles.
             expect(await token.hasRole(await token.DEFAULT_ADMIN_ROLE(), deployer.address)).to.be.true;
             expect(await token.hasRole(await token.PAUSER_ROLE(), deployer.address)).to.be.true;
-            expect(await token.hasRole(await token.BRIDGE_ROLE(), bridge.address)).to.be.true;
-        });
-
-        it("Should revert if bridge address is zero address during deployment", async () => {
-            await expect(
-                ethers.deployContract("MAGToken", [TOTAL_SUPPLY, ethers.ZeroAddress], deployer)
-            ).to.be.revertedWithCustomError(token, "ZeroAddress");
         });
     });
 
@@ -68,6 +60,7 @@ describe("MAG Token", function () {
             // Check final state.
             expect(await token.paused()).to.be.true;
         });
+
         it("Should allow to unpause", async () => {
             // Check initial state.
             expect(await token.paused()).to.be.false;
@@ -85,51 +78,7 @@ describe("MAG Token", function () {
             expect(await token.paused()).to.be.false;
         });
 
-        it("Should allow to transfer tokens only for admin or bridge when paused", async () => {
-            // Prepare data.
-            const amount = addDec(100);
-
-            // Pause.
-            await token.pause();
-
-            // Check that contract is on paused state.
-            expect(await token.paused()).to.be.true;
-
-            // Try to transfer tokens from `user` and expect to get error.
-            await expect(token.connect(user).transfer(bridge, amount)).to.be.revertedWithCustomError(
-                token,
-                "OnlyBridgeCanTransfer"
-            );
-
-            // Try to transfer tokens from `bridge` and get success.
-            const userBalance = await token.balanceOf(user);
-            await expect(token.connect(bridge).transfer(user, amount)).to.not.be.reverted;
-            expect(await token.balanceOf(user)).to.be.equal(userBalance + amount);
-
-            // Try to transfer tokens from `admin` and get success.
-            const userBalance2 = await token.balanceOf(user);
-            await expect(token.connect(deployer).transfer(user, amount)).to.not.be.reverted;
-            expect(await token.balanceOf(user)).to.be.equal(userBalance2 + amount);
-        });
-
-        it("Should allow to transfer tokens for all when not paused", async () => {
-            // Prepare data.
-            const amount = addDec(100);
-
-            // Check that contract is on not paused state.
-            expect(await token.paused()).to.be.false;
-
-            // Try to transfer tokens from `user` and get success.
-            const bridgeBalance = await token.balanceOf(bridge);
-            await expect(token.connect(user).transfer(bridge, amount)).to.not.be.reverted;
-            expect(await token.balanceOf(bridge)).to.be.equal(bridgeBalance + amount);
-        });
-
-        it("Should revert if caller does not have pauser role", async () => {
-            // Check that `user` is not pauser.
-            expect(await token.hasRole(await token.PAUSER_ROLE(), user.address)).to.be.false;
-
-            // Try to pause and expect to get error.
+        it("Should revert if caller is not an owner", async () => {
             await expect(token.connect(user).pause()).to.be.revertedWith(
                 "AccessControl: account" +
                     " " +
@@ -138,13 +87,8 @@ describe("MAG Token", function () {
                     (await token.PAUSER_ROLE()).toLocaleLowerCase()
             );
 
-            // Set state to paused.
-            await token.pause();
+            await token.connect(deployer).pause();
 
-            // Check that contract is on paused state.
-            expect(await token.paused()).to.be.true;
-
-            // Try to unpause from `user` and expect to get error.
             await expect(token.connect(user).unpause()).to.be.revertedWith(
                 "AccessControl: account" +
                     " " +
@@ -154,21 +98,28 @@ describe("MAG Token", function () {
             );
         });
 
-        it("Should revert when target state already set", async () => {
-            // Check that contract is on not paused state.
-            expect(await token.paused()).to.be.false;
-
-            // Set state to unpaused.
-            await expect(token.unpause()).to.be.revertedWith("Pausable: not paused");
-
-            // Set state to paused.
+        it("Should revert if state is already set", async () => {
             await token.pause();
 
-            // Check that contract is on paused state.
-            expect(await token.paused()).to.be.true;
-
-            // Set state to paused.
             await expect(token.pause()).to.be.revertedWith("Pausable: paused");
+
+            await token.unpause();
+
+            await expect(token.unpause()).to.be.revertedWith("Pausable: not paused");
+        });
+    });
+
+    describe("Transfer", function () {
+        it("Should revert to transfer when paused", async () => {
+            await token.pause();
+
+            await expect(token.transfer(user, addDec(1))).to.be.revertedWith(
+                "ERC20Pausable: token transfer while paused"
+            );
+
+            await token.unpause();
+
+            await token.transfer(user, addDec(1));
         });
     });
 });
